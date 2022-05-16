e6ab89ba6cc6bb4564f3b984614a03844ba49872
Anubhav <snehi.anubhav@tftus.com>
2022-05-16 15:15:21 +0530

    Update

--- src/components/Movie/RelatedMovieList.js
+++ src/components/Movie/RelatedMovieList.js
@@ -1,6 +1,6 @@
 import React from "react";
-import MovieCard from "./MovieCard";
+// import MovieCard from "./MovieCard";
 const RelatedMovieList = () => {
-  return <MovieCard />;
+  return <div></div>
 };
 export default RelatedMovieList;
--- src/store/actions/movie-action.js
+++ src/store/actions/movie-action.js
@@ -64,7 +64,6 @@ export const handleFetchMovies = () => {
             }
             dispatch(toggleLoading())
             dispatch(fetchMovies(movies))
-            console.log(movies)
         } catch (error) {
             console.log(error.message)
         }
