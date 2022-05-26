1fb263b095c34c54405d2ed48a313290edb241d9
Anubhav <snehi.anubhav@tftus.com>
2022-05-26 14:38:41 +0530

    Updated Craousel

--- src/components/MediaScreen/MediaScreen.js
+++ src/components/MediaScreen/MediaScreen.js
@@ -60,7 +60,7 @@ const MediaScreen = ({ movies, id, heading }) => {
       <div className={styles.heading}>{heading}</div>
       <div className={styles.leftIconDiv} onClick={scrollToLeft}>
         <img
-          src={leftScrollIcon}
+          src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMzIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTMuMjM3IDE3LjIzN3YtMi40NzRsMTQgMTRjLjY4NC42ODMuNjg0IDEuNzkgMCAyLjQ3NGExLjc0OCAxLjc0OCAwIDAgMS0yLjQ3NCAwbC0xNC0xNGExLjc0OCAxLjc0OCAwIDAgMSAwLTIuNDc0bDE0LTE0YTEuNzQ4IDEuNzQ4IDAgMCAxIDIuNDc0IDBjLjY4NC42ODMuNjg0IDEuNzkgMCAyLjQ3NGwtMTQgMTR6IiBmaWxsPSIjRUZGMUYxIi8+PC9zdmc+"
           alt="left_Scroll"
           className={styles.leftIcon}
         />
@@ -100,7 +100,7 @@ const MediaScreen = ({ movies, id, heading }) => {
       </div>
       <div className={styles.rightIconDiv} onClick={scrollToRight}>
         <img
-          src={rightScrollIcon}
+          src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMzIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTE0Ljc2MyAxNy4yMzd2LTIuNDc0bC0xNCAxNGExLjc0OCAxLjc0OCAwIDAgMCAwIDIuNDc0Yy42ODMuNjg0IDEuNzkuNjg0IDIuNDc0IDBsMTQtMTRhMS43NDggMS43NDggMCAwIDAgMC0yLjQ3NGwtMTQtMTRBMS43NSAxLjc1IDAgMCAwIC43NjMgMy4yMzdsMTQgMTR6IiBmaWxsPSIjRUZGMUYxIi8+PC9zdmc+"
           alt="left_Scroll"
           className={styles.rightIcon}
         />
